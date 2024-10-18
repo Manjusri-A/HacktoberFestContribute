822e03af15479b1dc1b8404199dd7027f9d2f870
Manjusri Arumugam <msri10792@gmail.com>
2024-10-18 18:06:29 +0530

    NewmanConvoy_recursion.cs

--- NewmanConvoy_recursion.cs
+++ NewmanConvoy_recursion.cs
@@ -1,27 +1,30 @@
+using System;
 
-// C# program to find nth element 
-// of Newman-Conway Sequence 
-using System; 
-  
-public class NewConvSeq { 
-      
-    // Using Recursion  
-    static int newmanconvoy(int n) 
-    { 
-        if (n == 1 || n == 2)            // Base case 
-            return 1; 
+public class NewConvSeq
+{
+    // Using Recursion
+    static int newmanConvoy(int n)
+    {
+        if (n == 1 || n == 2) // Base case
+            return 1;
+
+        // Correct recursive calls for Newman-Conway sequence
+        return newmanConvoy(newmanConvoy(n - 1)) + newmanConvoy(n - newmanConvoy(n - 1));
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Enter a position in the Newman-Conway sequence:");
+        string val = Console.ReadLine();
+
+        // Input validation
+        if (int.TryParse(val, out int num) && num > 0)
+        {
+            Console.WriteLine($"Newman-Conway Number corresponding to position {num} is: {newmanConvoy(num)}");
+        }
         else
-            return newmanconvoy(newmanconvoy(n - 1)) + newmanconvoy 
-                           (n - newmanconvoy(n - 1)); 
-    } 
-      
-    public static void Main() 
-    {  
-		string val;
-        val = Console.ReadLine();
-
-        int num = Convert.ToInt32(val);
-        Console.WriteLine("Newman Convoy Number Corresponding to entered position is:");
-        Console.Write(newmanconvoy(num)); 
-    } 
-} 
+        {
+            Console.WriteLine("Please enter a valid positive integer.");
+        }
+    }
+}
