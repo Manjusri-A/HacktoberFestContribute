a0072f8ed4eb08bc31063a8c96d01c66aa729329
Manjusri Arumugam <msri10792@gmail.com>
2024-10-18 18:04:40 +0530

    Update PowerUsingRecursion.cpp

--- PowerUsingRecursion.cpp
+++ PowerUsingRecursion.cpp
@@ -1,21 +1,32 @@
 #include <iostream>
 using namespace std;
-int calculatePower(int, int);
-int main()
-{
+
+int calculatePower(int base, int powerRaised);
+
+int main() {
     int base, powerRaised, result;
+
     cout << "Enter base number: ";
     cin >> base;
-    cout << "Enter power number(positive integer): ";
+
+    cout << "Enter power number (positive integer): ";
     cin >> powerRaised;
+
+    // Input validation for positive integers
+    if (powerRaised < 0) {
+        cout << "Power raised should be a non-negative integer." << endl;
+        return 1; // Exit the program with an error code
+    }
+
     result = calculatePower(base, powerRaised);
-    cout << base << "^" << powerRaised << " = " << result;
+    cout << base << "^" << powerRaised << " = " << result << endl;
+
     return 0;
 }
-int calculatePower(int base, int powerRaised)
-{
+
+int calculatePower(int base, int powerRaised) {
     if (powerRaised != 0)
-        return (base*calculatePower(base, powerRaised-1));
+        return (base * calculatePower(base, powerRaised - 1));
     else
-        return 1;
+        return 1; // Base case: any number to the power of 0 is 1
 }
